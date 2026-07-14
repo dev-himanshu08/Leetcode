cd7d420ecbdb16ebba282a29519e32a5490e97e5
Himanshu <notfound0786@gmail.com>
2026-07-14 21:51:03 +0530

    Time: 1 ms (87.04%), Space: 41.1 MB (51.85%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.kt
@@ -0,0 +1,29 @@
+class Solution {
+    fun divide(dividend: Int, divisor: Int): Int {
+        if (dividend == Int.MIN_VALUE && divisor == -1)
+            return Int.MAX_VALUE
+
+        val negative = (dividend < 0) xor (divisor < 0)
+
+        var dvd = kotlin.math.abs(dividend.toLong())
+        val dvs = kotlin.math.abs(divisor.toLong())
+
+        var result = 0L
+
+        while (dvd >= dvs) {
+
+            var temp = dvs
+            var multiple = 1L
+
+            while (dvd >= (temp shl 1)) {
+                temp = temp shl 1
+                multiple = multiple shl 1
+            }
+
+            dvd -= temp
+            result += multiple
+        }
+
+        return if (negative) -result.toInt() else result.toInt()
+    }
+}
