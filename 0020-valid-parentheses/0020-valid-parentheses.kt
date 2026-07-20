cc18be53deaad06ed0800f498c7fe900af0ddead
Himanshu <notfound0786@gmail.com>
2026-07-20 22:24:31 +0530

    Time: 19 ms (49.78%), Space: 45.7 MB (55.2%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.kt
@@ -0,0 +1,28 @@
+class Solution {
+    fun isValid(s: String): Boolean {
+        val stack = ArrayDeque<Char>()
+
+        for (ch in s) {
+            when (ch) {
+                '(', '{', '[' -> stack.addLast(ch)
+
+                ')' -> {
+                    if (stack.isEmpty() || stack.removeLast() != '(')
+                        return false
+                }
+
+                '}' -> {
+                    if (stack.isEmpty() || stack.removeLast() != '{')
+                        return false
+                }
+
+                ']' -> {
+                    if (stack.isEmpty() || stack.removeLast() != '[')
+                        return false
+                }
+            }
+        }
+
+        return stack.isEmpty()
+    }
+}
