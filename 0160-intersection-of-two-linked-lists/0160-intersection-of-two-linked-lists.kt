a12751cef70a04b9c4d0623174900ebd3e62bd2d
Himanshu <notfound0786@gmail.com>
2026-08-08 17:13:02 +0530

    Time: 185 ms (87.31%), Space: 54.5 MB (82.84%) - LeetHub

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.kt
@@ -0,0 +1,23 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+class Solution {
+    fun getIntersectionNode(headA: ListNode?, headB: ListNode?): ListNode? {
+        var p1 = headA
+        var p2 = headB
+
+        while (p1 != p2) {
+            p1 = if (p1 == null) headB else p1.next
+            p2 = if (p2 == null) headA else p2.next
+        }
+
+        return p1
+    }
+}
