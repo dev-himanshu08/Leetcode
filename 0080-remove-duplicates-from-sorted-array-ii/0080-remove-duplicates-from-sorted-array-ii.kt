22c05fab5f88f18e3067863770ee5bd7b0f28781
Himanshu <notfound0786@gmail.com>
2026-08-13 23:19:00 +0530

    Time: 178 ms (45.64%), Space: 47 MB (84.13%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun removeDuplicates(nums: IntArray): Int {
+        if (nums.size <= 2) return nums.size
+
+        var k = 2
+
+        for (i in 2 until nums.size) {
+            if (nums[i] != nums[k - 2]) {
+                nums[k] = nums[i]
+                k++
+            }
+        }
+
+        return k
+    }
+}
