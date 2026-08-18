be52be8a0eeaad264e5c27576f9bb21e2b028152
Himanshu <notfound0786@gmail.com>
2026-08-18 22:15:11 +0530

    Time: 5 ms (36.71%), Space: 70.5 MB (5.48%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.kt
@@ -0,0 +1,27 @@
+class Solution {
+    fun rotate(nums: IntArray, k: Int) {
+
+        val n = nums.size
+        val steps = k % n
+
+        reverse(nums, 0, n - 1)
+
+        reverse(nums, 0, steps - 1)
+
+        reverse(nums, steps, n - 1)
+    }
+
+    private fun reverse(nums: IntArray, start: Int, end: Int) {
+        var left = start
+        var right = end
+
+        while (left < right) {
+            val temp = nums[left]
+            nums[left] = nums[right]
+            nums[right] = temp
+
+            left++
+            right--
+        }
+    }
+}
