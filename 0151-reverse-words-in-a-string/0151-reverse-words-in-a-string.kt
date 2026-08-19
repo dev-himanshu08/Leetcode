74b58acfdeafa443576b45f92a0942a9d10d549e
Himanshu <notfound0786@gmail.com>
2026-08-19 22:48:25 +0530

    Time: 21 ms (50%), Space: 46.4 MB (21.8%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun reverseWords(s: String): String {
+        return s.trim()
+            .split(Regex("\\s+"))
+            .reversed()
+            .joinToString(" ")
+    }
+}
