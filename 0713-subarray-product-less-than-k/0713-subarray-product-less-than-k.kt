619e350a7ef099a321d41a3bf2aaadbd01d4fb1b
Himanshu <notfound0786@gmail.com>
2026-08-31 21:52:56 +0530

    Time: 3 ms (98.25%), Space: 49.7 MB (33.33%) - LeetHub

---
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.kt
@@ -0,0 +1,22 @@
+class Solution {
+    fun numSubarrayProductLessThanK(nums: IntArray, k: Int): Int {
+        if (k <= 1) return 0
+
+        var left = 0
+        var product = 1
+        var count = 0
+
+        for (right in nums.indices) {
+            product *= nums[right]
+
+            while (product >= k) {
+                product /= nums[left]
+                left++
+            }
+
+            count += right - left + 1
+        }
+
+        return count
+    }
+}
